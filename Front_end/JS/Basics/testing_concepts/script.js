a21e488285b415259892b9fe73bf1855fbef0826
Aaditya <aadityanayak7@gmail.com>
2022-02-11 20:14:08 +0530

    Changed body background

--- Front_end/JS/Basics/testing_concepts/script.js
+++ Front_end/JS/Basics/testing_concepts/script.js
@@ -1,9 +1,1 @@
 "use strict"
-
-let heading = document.getElementById("firstDiv");
-
-heading.style.transition = "all 1s";
-
-heading.addEventListener('click', (event) => {
-    heading.innerHTML = "<h3>This is heading</h3>"
-})
